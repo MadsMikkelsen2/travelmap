42d759f439c06b93d3940752c2181232e7351078
MadsMikkelsen2 <tchi33969@gmail.com>
2024-07-17 07:00:59 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,38 @@
 # 必要なライブラリをインポートする
 import streamlit as st
-import random
+import requests
 
-# ページのタイトルを設定する
-st.title('サイコロシミュレータ')
+# Streamlitアプリケーションのタイトルを設定する
+st.title('現在地を地図上に表示する')
 
-# ヘッダーを表示する
-st.header('サイコロを振ろう！')
+# IPアドレスを取得するAPIのURL
+ip_api_url = 'https://api.ipify.org?format=json'
 
-# 「振る」ボタンを作成する
-if st.button('振る'):
-    # 1から6の間のランダムな整数を生成する
-    dice_value = random.randint(1, 6)
-    # サイコロの出目を表示する
-    st.write(f'出た目は: {dice_value}')
+# IPアドレスを取得する関数
+def get_ip():
+    response = requests.get(ip_api_url)
+    ip_data = response.json()
+    return ip_data['ip'] if 'ip' in ip_data else None
 
+# IPアドレスから現在の位置情報を取得するAPIのURL
+location_api_url = 'http://ip-api.com/json/'
+
+# 現在の位置情報を取得する関数
+def get_location(ip):
+    response = requests.get(location_api_url + ip)
+    location_data = response.json()
+    return location_data
+
+# 現在のIPアドレスを取得する
+ip_address = get_ip()
+
+# IPアドレスが取得できた場合
+if ip_address:
+    # 現在の位置情報を取得する
+    location_data = get_location(ip_address)
+    
+    # 地図を表示する
+    st.map((location_data['lat'], location_data['lon']))
+
+    # 位置情報の詳細を表示する
+    st.write(f'現在地の情報
