a7c5d41b2ba468eb3ac0c1ef34aeb74d39718ffc
MadsMikkelsen2 <tchi33969@gmail.com>
2024-07-17 07:09:40 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,23 +5,19 @@ import requests
 # Streamlitアプリケーションのタイトルを設定する
 st.title('現在地を地図上に表示する')
 
-# IPアドレスを取得するAPIのURL
-ip_api_url = 'https://api.ipify.org?format=json'
 
-# IPアドレスを取得する関数
-def get_ip():
-    response = requests.get(ip_api_url)
-    ip_data = response.json()
-    return ip_data['ip'] if 'ip' in ip_data else None
-
-# IPアドレスから現在の位置情報を取得するAPIのURL
+# 現在の位置情報を取得するAPIのURL
 location_api_url = 'http://ip-api.com/json/'
 
 # 現在の位置情報を取得する関数
 def get_location(ip):
     response = requests.get(location_api_url + ip)
-    location_data = response.json()
-    return location_data
+    if response.status_code == 200:
+        location_data = response.json()
+        return location_data
+    else:
+        st.error(f'位置情報を取得できませんでした。ステータスコード: {response.status_code}')
+        return None
 
 # 現在のIPアドレスを取得する
 ip_address = get_ip()
@@ -31,17 +27,19 @@ if ip_address:
     # 現在の位置情報を取得する
     location_data = get_location(ip_address)
     
-    # 地図を表示する
-    st.map((location_data['lat'], location_data['lon']))
-
-    # 位置情報の詳細を表示する
-    st.write(f'現在地の情報:')
-    st.write(f'都市: {location_data["city"]}')
-    st.write(f'国: {location_data["country"]}')
-    st.write(f'緯度: {location_data["lat"]}')
-    st.write(f'経度: {location_data["lon"]}')
-    st.write(f'ISP: {location_data["isp"]}')
-
+    if location_data:
+        # 地図を表示する
+        st.map((location_data['lat'], location_data['lon']))
+        
+        # 位置情報の詳細を表示する
+        st.write(f'現在地の情報:')
+        st.write(f'都市: {location_data["city"]}')
+        st.write(f'国: {location_data["country"]}')
+        st.write(f'緯度: {location_data["lat"]}')
+        st.write(f'経度: {location_data["lon"]}')
+        st.write(f'ISP: {location_data["isp"]}')
+    else:
+        st.write('位置情報が取得できませんでした。')
 else:
     st.write('IPアドレスが取得できませんでした。')
 
