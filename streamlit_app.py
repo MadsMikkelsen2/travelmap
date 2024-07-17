13d860166c8e82a45c0b91d3e1901d2e4530ecb4
MadsMikkelsen2 <tchi33969@gmail.com>
2024-07-17 08:46:51 +0000

    ｓ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,11 @@
+
 import geocoder
 
 def get_current_location():
-    # geocoderを使用してIPアドレスから位置情報を取得する例
     g = geocoder.ip('me')
     return g.latlng
 
 def main():
-    # 現在の位置情報を取得
     location = get_current_location()
     if location:
         latitude, longitude = location
