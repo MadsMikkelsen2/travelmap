3f24895a23ea11539c12b8ebc496e6ce1a81c701
MadsMikkelsen2 <tchi33969@gmail.com>
2024-07-17 07:30:40 +0000

    ｈ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,30 @@
 # 必要なライブラリをインポートする
 import streamlit as st
+import requests
 import folium
-from streamlit_folium import folium_static  # 必要に応じて追加
-
-# 以下にアプリケーションのコードを記述します
-
+from streamlit_folium import folium_static
 
 # Streamlitアプリケーションのタイトルを設定する
-st.title('日本地図を表示する')
+st.title('現在の位置を地図上に表示する')
 
-# 日本の中心座標を指定する（おおよその位置）
-japan_center = (36.2048, 138.2529)
+# IP-APIのURL
+location_api_url = 'http://ip-api.com/json/'
 
-# Foliumの地図オブジェクトを作成する
-m = folium.Map(location=japan_center, zoom_start=6)
+# 現在の位置情報を取得する関数
+def get_location(ip):
+    response = requests.get(location_api_url + ip)
+    if response.status_code == 200:
+        location_data = response.json()
+        return location_data
+    else:
+        st.error(f'位置情報を取得できませんでした。ステータスコード: {response.status_code}')
+        return None
 
-# Streamlitのウィジェットに地図を表示する
-folium_static(m)
+# 現在のIPアドレスを取得する
+ip_address = st.experimental_get_query_params().get('ip', None) or st.experimental_get_query_params().get('ip_address', None)
+if ip_address is None:
+    ip_address = st.experimental_get_query_params().get('ip', None) or st.experimental_get_query_params().get('ip_address', None)
+    if not ip_address:
+        ip_address = get_ip_address()
+    デフ゛
 
-
-
-
-# 地図を作成する
-m = folium.Map(location=[36.2048, 138.2529], zoom_start=5)  # 初期位置を日本の中心に設定する
-
-
-# Streamlitで地図を表示する
-folium_static(m)
